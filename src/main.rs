32053b2a117bbad500853c798dc241fb82748bd5
Furry <ender@thefluegges.com>
2022-04-21 16:29:39 -0400

    tesseract

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // use tokio::sync::mpsc;
-use std::sync::mpsc;
+use std::{ sync::mpsc, path, fs };
 
 pub mod scanner;
 
@@ -10,21 +10,29 @@ pub struct Task {
 
 #[tokio::main]
 async fn main() {
+    let save_image: bool = std::env::args().nth(1).unwrap_or("true".to_owned()).parse().unwrap();
+
+    // If the directory './images' doesnt exist, create it.
+    if save_image && !path::Path::new("./images").exists() {
+        fs::create_dir("./images").unwrap();
+    }
+
     let threads = 4;
     let (tx, rx) = mpsc::channel::<Task>();
 
     for _ in 0..threads {
         // Tokio oneshot
         let c = tx.clone();
+        let save = save_image.clone();
         tokio::spawn(async move {
-            scanner::scrape(c).await;
+            scanner::scrape(c, save).await;
         });
     }
 
     loop {
         match rx.try_recv() {
             Ok(task) => {
-                println!("{:?}", task);
+                println!("{:?}", task.id);
             }
             Err(_) => {}
         }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,13 +1,17 @@
 use rand::{distributions::Alphanumeric, Rng};
+use tokio::io::AsyncWriteExt;
 // use tokio::sync::mpsc::Sender;
-use std::sync::mpsc::Sender;
-use reqwest::{ StatusCode, Client };
+use std::{sync::mpsc::Sender, io::Write};
+use reqwest::{ StatusCode, Client, redirect::Policy };
 
 use crate::Task;
 
-pub async fn scrape(tx: Sender<Task>) {
+pub async fn scrape(tx: Sender<Task>, save: bool) {
     // let rng = rand::thread_rng();
-    let client = Client::new();
+    let client = Client::builder()
+        .redirect(Policy::none())
+        .build()
+        .unwrap();
 
     loop {
         let sample = rand::thread_rng()
@@ -17,13 +21,21 @@ pub async fn scrape(tx: Sender<Task>) {
 
         let id = String::from_utf8(sample.clone()).unwrap();
         
-        let r = client.get("https://i.imgur.com/".to_owned() + &id)
+        let r = client.get("https://i.imgur.com/".to_owned() + &id + ".png")
             .send()
             .await
             .unwrap();
 
         match r.status() {
             StatusCode::OK => {
+                if save {
+                    let bytes = r.bytes().await.unwrap();
+                    let mut f = tokio::fs::File::create("./images/".to_owned() + &id + ".png")
+                        .await
+                        .unwrap();
+                    f.write_all(&bytes).await.unwrap();
+                }
+
                 tx.send(Task {
                     id: sample.try_into().unwrap()
                 }).unwrap();
@@ -31,4 +43,8 @@ pub async fn scrape(tx: Sender<Task>) {
             _ => {}
         }
     }
+}
+
+pub async fn save() {
+
 }
